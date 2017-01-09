e5c2bce5320113daadf4a27c0d1a60d34dc4609b
HaritDivyesh <haritdivyesh@gmail.com>
2017-01-09 12:33:21 -0500

    Angular Login, Errorwith controller and model

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -7,8 +7,13 @@
     function config($routeProvider) {
 
     $routeProvider
+        .when("/", {
+            templateUrl: "view/home.html"
+        })
         .when("/login", {
-            templateUrl: "view/user/login.view.client.html"
+            templateUrl: "view/user/login.view.client.html",
+            controller: "LoginController",
+            controllerAs: "model"
         })
         .when("/register", {
             templateUrl: "view/user/register.view.client.html"
@@ -16,6 +21,9 @@
         .when("/profile", {
             templateUrl: "view/user/profile.view.client.html"
         })
+        .otherwise({
+            redirectTo: "/login"
+        });
     }
 
 })();
---
+++ public/assignment/view/user/user.controller.client.js
@@ -0,0 +1,40 @@
+(function(){
+
+    angular
+        .module("WebAppMaker")
+        .controller("LoginController", LoginController);
+
+    function LoginController() {
+
+        var vm=this;
+
+        var users= [
+
+            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+
+        ] ;
+
+        vm.login= function(username, password) {
+
+            console.log(username);
+            console.log(password);
+
+            for (var i in users) {
+                if (users[i].username === username && users[i].password === password) {
+
+                    console.log("YAY");
+
+                }
+                else {
+
+                    vm.test = "User not found";
+                }
+            }
+        }
+
+    }
+
+})();
